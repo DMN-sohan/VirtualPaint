b3a162cb6ffda161a535b1ca40f7cd16faef1b19
DMN-sohan <65285526+DMN-sohan@users.noreply.github.com>
2022-07-24 21:42:18 +0530

    Update color_picker.py

--- color_picker.py
+++ color_picker.py
@@ -9,15 +9,17 @@ cap.set(10,150)
 
 def emp(a):
     pass
-
+def print_toggle(a):
+    print("Clicked button")
 cv2.namedWindow("HSV")
 cv2.resizeWindow("HSV",640,240)
 cv2.createTrackbar("h min","HSV",0,255,emp)
-cv2.createTrackbar("h max","HSV",0,255,emp)
 cv2.createTrackbar("s min","HSV",0,255,emp)
-cv2.createTrackbar("s max","HSV",0,255,emp)
 cv2.createTrackbar("v min","HSV",0,255,emp)
+cv2.createTrackbar("h max","HSV",0,255,emp)
+cv2.createTrackbar("s max","HSV",0,255,emp)
 cv2.createTrackbar("v max","HSV",0,255,emp)
+
 cap=cv2.VideoCapture(0)
 
 while True:
@@ -25,11 +27,12 @@ while True:
     imgHSV=cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
 
     h_min = cv2.getTrackbarPos("h min", "HSV")
-    h_max = cv2.getTrackbarPos("h max", "HSV")
     s_min = cv2.getTrackbarPos("s min", "HSV")
-    s_max = cv2.getTrackbarPos("s max", "HSV")
     v_min = cv2.getTrackbarPos("v min", "HSV")
+    h_max = cv2.getTrackbarPos("h max", "HSV")
+    s_max = cv2.getTrackbarPos("s max", "HSV")
     v_max = cv2.getTrackbarPos("v max", "HSV")
+
     lower=np.array([h_min,s_min,v_min])
     upper=np.array([h_max,s_max,v_max])
     mask=cv2.inRange(imgHSV,lower,upper)
@@ -39,11 +42,19 @@ while True:
     hstack=np.hstack([img,mask,result])
     cv2.imshow("1",hstack)
 
-    if cv2.waitKey(1) & 0xFF ==ord('s'):
+    
+
+    if cv2.waitKey(1) & 0xFF == ord('s'):
         break
+    elif cv2.waitKey(1) == ord('q'):
+        print([h_min,s_min,v_min,h_max,s_max,v_max])
+        f = open("values.txt", "w")
+        f.write(str(h_min)+'\n')
+        f.write(str(s_min)+'\n')
+        f.write(str(v_min)+'\n')
+        f.write(str(h_max)+'\n')
+        f.write(str(s_max)+'\n')
+        f.write(str(v_max)+'\n')
+        f.close()   
 cap.release()
 cv2.destroyAllWindows()
-
-
-
-
